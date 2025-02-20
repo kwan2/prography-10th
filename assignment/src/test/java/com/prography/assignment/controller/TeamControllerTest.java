9eb81b93a3299021670c5f7bf2b72febe49f7055
정관희 <kwanh22@daou.co.kr>
2025-02-20 19:07:23 +0900

    이름 변경

---
+++ assignment/src/test/java/com/prography/assignment/controller/TeamControllerTest.java
@@ -0,0 +1,35 @@
+package com.prography.assignment.controller;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import com.prography.assignment.api.userRoom.dto.request.TeamChangeRequest;
+import com.prography.assignment.global.dto.ApiResponse;
+import com.prography.assignment.util.ResponseTestTemplate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.web.servlet.MvcResult;
+
+public class TeamControllerTest extends ResponseTestTemplate {
+
+    private static final Logger log = LoggerFactory.getLogger(UserControllerTest.class);
+
+    @Test
+    @DisplayName("팀 변경 API")
+    public void changeTeamTest() throws Exception {
+
+        Integer userId = 1, roomId = 1;
+
+        TeamChangeRequest teamChangeRequest = new TeamChangeRequest(userId);
+
+        MvcResult mvcResult = performWithParam("/team/{roomId}", roomId.toString(), teamChangeRequest, HttpMethod.PUT);
+
+        ApiResponse<Void> apiResponse = ofTest(mvcResult, Void.class);
+
+        assertThat(apiResponse.getCode()).isEqualTo(200);
+        assertThat(apiResponse.getMessage()).isEqualTo("API 요청이 성공했습니다.");
+
+    }
+}
