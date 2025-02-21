e438d9720110de878e89e4bbf584f9b9cbfd8fed
정관희 <kwanh22@daou.co.kr>
2025-02-21 11:33:34 +0900

    swagger 적용 3

--- assignment/src/main/java/com/prography/assignment/api/user/dto/UserDto.java
+++ assignment/src/main/java/com/prography/assignment/api/user/dto/UserDto.java
@@ -1,6 +1,7 @@
 package com.prography.assignment.api.user.dto;
 
 import com.prography.assignment.api.user.domain.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Builder;
 
@@ -9,12 +10,19 @@ import java.time.format.DateTimeFormatter;
 
 @Builder(access = AccessLevel.PRIVATE)
 public record UserDto(
+        @Schema(description = "유저 ID", example = "1")
         Integer id,
+        @Schema(description = "Faker API 유저 ID", example = "1")
         Integer fakerId,
+        @Schema(description = "유저 이름", example = "정프로")
         String name,
+        @Schema(description = "유저 이메일", example = "prography@prography.com")
         String email,
+        @Schema(description = "유저 상태", example = "ACTIVE")
         String status,
+        @Schema(description = "유저 정보 생성 일", example = "2025-02-21 13:14:15")
         String createAt,
+        @Schema(description = "방 정보 변경 일", example = "2025-02-22 13:14:15")
         String updateAt
 ) {
     public static UserDto from(User user) {
--- assignment/src/main/java/com/prography/assignment/api/user/dto/response/UserTotalResponse.java
+++ assignment/src/main/java/com/prography/assignment/api/user/dto/response/UserTotalResponse.java
@@ -1,15 +1,17 @@
 package com.prography.assignment.api.user.dto.response;
 
 import com.prography.assignment.api.user.dto.UserDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.util.List;
 
 @Builder
 public record UserTotalResponse(
-    Integer totalElements,
-    Integer totalPages,
-    List<UserDto> userList
+
+        @Schema(description = "유저 전체 수", example = "10") Integer totalElements,
+        @Schema(description = "유저 전체 페이지 수", example = "2")Integer totalPages,
+        @Schema(description = "유저 상세 정보 리스트") List<UserDto> userList
 ) {
 
     public static UserTotalResponse of(Integer totalElements, Integer totalPages, List<UserDto> userList) {
